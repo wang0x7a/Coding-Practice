41ece06730c58cee1dc9c9337bf15f78fe066a02
wang0x7a <zhexwang@gmail.com>
2013-09-02 23:23:09 +0800

    second solution to P0106

--- cracking-the-coding-interview/01-arrays-and-strings/P0106.java
+++ cracking-the-coding-interview/01-arrays-and-strings/P0106.java
@@ -19,13 +19,41 @@ public class P0106 {
             matrix[end - offset][end] = matrix[end][start + offset];
             matrix[end][start + offset] = tmp;
          }
+
       }
+      return matrix;
+   }
+
+   /* Solution 2: touch each element twice */
+   public static int[][] rotate2(int[][] matrix) {
+      int n = matrix.length;
+      matrix = transpose(matrix);
+
+      for (int i = 0; i < n / 2; i++)
+         for (int j = 0; j < n; j++) {
+            int tmp = matrix[i][j];
+            matrix[i][j] = matrix[n - 1 - i][j];
+            matrix[n - 1 - i][j] = tmp;
+         }
+
+      return matrix;
+   }
+
+   /* swap for matrix elements */
+   private static int[][] transpose(int[][] matrix) {
+      int n = matrix.length;
+      for (int i = 1; i < n; i++)
+         for (int j = 0; j < i; j++) {
+            int tmp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = tmp;
+         }
       return matrix;
    }
 
    public static void main(String[] args) {
       int[][] matrix = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}};
-      matrix = P0106.rotate(matrix);
+      matrix = P0106.rotate2(matrix);
       for (int i = 0; i < matrix.length; i++) {
          for (int j = 0; j < matrix[0].length; j++)
             System.out.print(matrix[i][j] + " ");

e6d2e9dbe9d762cf939f15273752b9d1ee257372
Zhe Wang <zhewang@redhat.com>
2013-09-10 17:54:16 +0800

    solution to P0202

---
+++ cracking-the-coding-interview/02-linked-lists/P0202.java
@@ -0,0 +1,71 @@
+/* 2.2 Implement an algoritym to find the nth to last element of a singly
+ * linked list.
+ */
+import java.util.NoSuchElementException;
+
+public class P0202 {
+   private class Node {
+      int data;
+      Node next;
+   }
+
+   public static Node nth2Last(Node head, int n) {
+      Node slow = head.next;
+      Node fast = head.next;
+
+      for (int i = n; i > 0; i--, fast = fast.next) {
+         if (fast == null) {
+            throw new NoSuchElementException("n exceeds the length of the list");
+         }
+      }
+
+     while (fast != null) {
+        slow = slow.next;
+        fast = fast.next;
+     }
+
+     return slow;
+   }
+
+      private Node initialize(int[] data) {
+      Node head = new Node();
+      head.data = Integer.MIN_VALUE;
+      head.next = null;
+
+      if (data == null) {
+         throw new NoSuchElementException("Empty inputs");
+      }
+
+      Node current = head;
+      int len = data.length;
+      for (int i = 0; i < len; i++) {
+         Node tmp = new Node();
+         tmp.data = data[i];
+         tmp.next = null;
+         current.next = tmp;
+         current = tmp;
+      }
+      return head;
+   }
+
+   private String toString(Node list) {
+      StringBuilder s = new StringBuilder();
+      Node current = list;
+      while (current != null) {
+         s.append(current.data + " ");
+         current = current.next;
+      }
+
+      return s.toString();
+   }
+
+   public static void main(String[] args) {
+      int[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12};
+      P0202 p0202 = new P0202();
+      Node list = p0202.initialize(data);
+
+      //Node node = nth2Last(list, 13);
+      Node node = nth2Last(list, 3);
+      System.out.println(node.data);
+   }
+}

28d75e6a6f39d2d5b8f6dd982e2b91790c8b203a
Zhe Wang <zhewang@redhat.com>
2013-09-11 16:36:17 +0800

    second solution to P0204

--- cracking-the-coding-interview/02-linked-lists/P0204.java
+++ cracking-the-coding-interview/02-linked-lists/P0204.java
@@ -11,58 +11,102 @@
 import java.util.NoSuchElementException;
 
 public class P0204 {
-  private class Node {
-    private int digit;
-    private Node next;
-  }
+   private class Node {
+     private int digit;
+     private Node next;
+   }
 
-  public Node add(Node a, Node b) {
-    if (a.next == null && b.next != null) return b;
-    if (a.next != null && b.next == null) return a;
-    if (a.next == null && b.next == null) return null;
+   // Lessons:
+   // Pay attention to the movement of the pointers, for example,
+   // rr starts from result, and we need to initialize a new node for rr.next
+   //
+   // Analysis:
+   // The space complexity of this solution is O(N), where N is the length of
+   // the longer list. Note that the lists are mutable here, we could reduce 
+   // the complexity to O(1) by modifying the input directly.
+   public Node addWithBuffer(Node a, Node b) {
+      if (a.next == null && b.next != null) return b;
+      if (a.next != null && b.next == null) return a;
+      if (a.next == null && b.next == null) return null;
 
-    Node result = new Node();
-    result.digit = Integer.MIN_VALUE;
-    result.next = null;
-    // runner for a
-    Node ra = a.next;
-    // runner for b
-    Node rb = b.next;
-    // runner for result
-    Node rr = result;
+      Node result = new Node();
+      result.digit = Integer.MIN_VALUE;
+      result.next = null;
+      // runner for a
+      Node ra = a.next;
+      // runner for b
+      Node rb = b.next;
+      // runner for result
+      Node rr = result;
+
+      int carrier = 0;
+      int tmp = 0;
+      while (ra != null && rb != null) {
+         Node node = new Node();
+         tmp = ra.digit + rb.digit + carrier;
+         node.digit = tmp % 10;
+         carrier = tmp / 10;
+
+         rr.next = node;
+         ra = ra.next;
+         rb = rb.next;
+         rr = rr.next;
+      }
+
+      if (ra == null && rb != null) {
+         System.out.println("ra == null && rb != null");
+         Node node = new Node();
+         node.digit = rb.digit + carrier;
+         rr.next = node;
+         node.next = rb.next;
+      }
+      else if (ra != null && rb == null) {
+         Node node = new Node();
+         node.digit = ra.digit + carrier;
+         rr.next = node;
+         node.next = ra.next;
+      }
  
-    int carrier = 0;
-    int tmp = 0;
-    while (ra != null && rb != null) {
-      Node node = new Node();
-      tmp = ra.digit + rb.digit + carrier;
-      node.digit = tmp % 10;
-      carrier = tmp / 10;
+      return result;
+   }
 
-      rr.next = node;
-      ra = ra.next;
-      rb = rb.next;
-      rr = rr.next;
-    }
+   public Node addWithoutBuffer(Node a, Node b) {
+      if (a.next == null && b.next != null) return b;
+      if (a.next != null && b.next == null) return a;
+      if (a.next == null && b.next == null) return null;
 
-    if (ra == null && rb != null) {
-      System.out.println("ra == null && rb != null");
-      Node node = new Node();
-      node.digit = rb.digit + carrier;
-      rr.next = node;
-      node.next = rb.next;
-    }
-    else if (ra != null && rb == null) {
-      Node node = new Node();
-      node.digit = ra.digit + carrier;
-      rr.next = node;
-      node.next = ra.next;
-    }
+      // Use a to record the result
+      Node ra = a.next;
+      Node rb = b.next;
 
-    return result;
-  }
+      int carrier = 0;
+      int tmp = 0;
+      while (ra != null && rb != null) {
+         tmp = ra.digit + rb.digit + carrier;
+         System.out.print(tmp + " ");
+         rb.digit = ra.digit = tmp % 10;
+         carrier = tmp / 10;
 
-   private Node initialize(int[] digit) {
+         ra = ra.next;
+         rb = rb.next;
+      }
+
+      if (ra == null && rb != null) {
+         rb.digit += carrier;
+         return b;
+      }
+      /* The cases that (ra == null && rb == null) and
+       * (ra != null && rb == null) can be merged, since when ra and rb
+       * are both null, we could return either of them. (they were modified
+       * simultaneously).
+       */
+      else {
+         ra.digit += carrier;
+         return a;
+      }
+   }
+
+   public Node initialize(int[] digit) {
       Node head = new Node();
       head.digit = Integer.MIN_VALUE;
       head.next = null;
@@ -83,7 +127,7 @@ public class P0204 {
       return head;
    }
 
-   private String toString(Node list) {
+   public String toString(Node list) {
       StringBuilder s = new StringBuilder();
       Node current = list;
       while (current != null) {
@@ -95,14 +139,16 @@ public class P0204 {
    }
 
    public static void main(String[] args) {
-      //int[] n1 = {3, 1, 5};
-      int[] n1 = {3};
+      int[] n1 = {3, 1, 5};
+      //int[] n1 = {3};
       int[] n2 = {5, 5, 2};
       P0204 p0204 = new P0204();
       Node a = p0204.initialize(n1);
       Node b = p0204.initialize(n2);
-      Node result = p0204.add(a, b);
-      System.out.println(p0204.toString(result));
+      //Node result1 = p0204.addWithBuffer(a, b);
+      Node result2 = p0204.addWithoutBuffer(a, b);
+      //System.out.println(p0204.toString(result1));
+      System.out.println(p0204.toString(result2));
    }
 
 }

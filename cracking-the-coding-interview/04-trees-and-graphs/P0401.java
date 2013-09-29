ac6b77e16ecfbdd6743e394d494bfd6b266874fc
Zhe Wang <zhewang@redhat.com>
2013-09-29 15:20:19 +0800

    bug fix

--- cracking-the-coding-interview/04-trees-and-graphs/P0401.java
+++ cracking-the-coding-interview/04-trees-and-graphs/P0401.java
@@ -11,14 +11,38 @@ public class P0401 {
    }
 
    public boolean isBalanced(Node root) {
-      if (root == null) return false;
+      if (root == null) return true;
 
-      return Math.abs(height(root.left) - height(root.right)) <= 1; 
+      int leftHeight = height(root.left);
+      int rightHeight = height(root.right);
+
+      boolean isRootBalanced = Math.abs(leftHeight - rightHeight) <= 1;
+
+      if (isRootBalanced)
+         return isBalanced(root.left) && isBalanced(root.right);
+      else
+         return false;
+
+      // return isRootBalanced && isBalanced(root.left) && isBalanced(root.right);
    }
 
    public int height(Node x) {
       if (x == null) return 0;
 
       return 1 + Math.max(height(x.left), height(x.right));
+   }
+
+   // an improved solution:
+   // 1) One of the problems of isBalanced above is that, the space complexity
+   // is consistently O(lgN), since we need to check from the root.
+   // Actually, if a subtree is unbalanced, the whole tree will be as well.
+   // Therefore, we could potentially reduce the space complexity by checking
+   // if one subtree is balanced, and will return false once we find an
+   // unbalanced one.
+   // 2) every time we calculate height, the heights of subtrees (subtrees of 
+   // subtrees) are repeatedly calculated, we coudl avaoid this redundancy by
+   // adding extra variables to record the heights of subtrees.
+   public boolean isBalanced2(Node root) {
+      if (root == null) return true;
    }
 }

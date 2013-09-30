7f6439b31d670c70938e33aea16abde1a14552f7
Zhe Wang <zhewang@redhat.com>
2013-09-30 14:01:23 +0800

    finally solve P0404

--- cracking-the-coding-interview/04-trees-and-graphs/P0404.java
+++ cracking-the-coding-interview/04-trees-and-graphs/P0404.java
@@ -35,6 +35,18 @@ public class P0404 {
       }
    }
 
+   public void levelOrder(Node root, int level, ArrayList<List<Node>> list) {
+      if (root == null) return;
+
+      // initialize the list at depth D
+      if (level > list.size()) list.add(new LinkedList<Node>());
+
+      // in order traversal to visit all the nodes
+      levelOrder(root.left, level + 1, list);
+      list.get(level - 1).add(root);
+      levelOrder(root.right, level + 1, list);
+   }
+
    public Node buildTree() {
       Node root = new Node(1);
       root.left = new Node(2);
@@ -51,9 +63,23 @@ public class P0404 {
       P0404 p0404 = new P0404();
       Node root = p0404.buildTree();
 
+      System.out.println("Solution #1:");
       for (int i = 1; i <= 4; i++) {
          List<Node> list = new LinkedList<Node>();
          p0404.levelOrder(root, i, 1, list);
+         Iterator<Node> iter = list.iterator();
+         while (iter.hasNext()) {
+            System.out.print(iter.next().value + " ");
+         }
+         System.out.println();
+      }
+
+      System.out.println("Solution #2:");
+      ArrayList<List<Node>> list2 = new ArrayList<List<Node>>();
+      p0404.levelOrder(root, 1, list2);
+
+      for (int i = 0; i < list2.size(); i++) {
+         List<Node> list = list2.get(i);
          Iterator<Node> iter = list.iterator();
          while (iter.hasNext()) {
             System.out.print(iter.next().value + " ");

25821f33ae271fbf76033b304b59353ba65fc1cc
wang0x7a <zhexwang@gmail.com>
2014-02-12 01:37:46 +0800

    AC

--- dsalgo/01-linear-lists/p0213.c
+++ dsalgo/01-linear-lists/p0213.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
 #include <math.h>
 
@@ -7,17 +8,42 @@
  * */
 #define MAX_GROUP 32000
 
-void main() {
-  int group[MAX_GROUP];
+void construct_groups(int *group);
+int solve(int index, int group[]);
+
+int main() {
+  //int group[MAX_GROUP];
+  int *group = malloc(sizeof(int) * MAX_GROUP); 
+
+  construct_groups(group);
+
+  int case_num, index;
+  scanf("%d", &case_num);
+
+  while (case_num-- > 0) {
+    scanf("%d", &index);
+    printf("%d\n", solve(index, group));
+  }
+
+  return 0;
+}
+
+void construct_groups(int *group) {
   group[0] = 0;
 
-  int index = 80;
   int i;
+  int acc = 0;
   /* IMPORTANT: i++ is exected before index > 0 */
-  for (i = 1; index > 0; i++) {
-    group[i] = group[i - 1] + (int)log10(i * 1.0) + 1; 
+  for (i = 1; acc >= 0; i++) { 
+    group[i] = group[i - 1] + (int)log10(i * 1.0) + 1;
+    acc += group[i];
+  } 
+}
+
+int solve(int index, int* group) {
+  int i;
+  for (i = 1; index > 0; i++)
     index -= group[i];
-  } // for
 
   /* That the for loop stops means that the traget digit is in
    * group[i] with the updated index [1, group[i]]
@@ -32,13 +58,10 @@ void main() {
 
   /* The digit should be some digit of the integer j*/
   index -= group[j - 1];
-  int n = (int)log10(j * 1.0) + 1 - index + 1;
+  int n = (int)log10(j * 1.0) + 1 - index + 1; 
   /* reverse index for mod op: for a 4-digit num, index = 1 => n = 4 */
-  while (n > 0) {
-    j %= 10;
-    n--;
-  }
+  j /= pow(10, n - 1);
+  j %= 10;
 
-  printf("%d\n", j);
-
+  return j;
 }

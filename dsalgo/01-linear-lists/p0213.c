f60a46c5db2add29d79aaa082ded82257ae5f164
wang0x7a <zhexwang@gmail.com>
2014-02-11 22:56:28 +0800

    for a single input

--- dsalgo/01-linear-lists/p0213.c
+++ dsalgo/01-linear-lists/p0213.c
@@ -11,16 +11,34 @@ void main() {
   int group[MAX_GROUP];
   group[0] = 0;
 
-  int i, acc = 0;
-  for (i = 1; acc <= INT_MAX; i++) {
-    group[i] = group[i - 1] + (int)log10(i) + 1; 
-    acc += group[i];
+  int index = 80;
+  int i;
+  /* IMPORTANT: i++ is exected before index > 0 */
+  for (i = 1; index > 0; i++) {
+    group[i] = group[i - 1] + (int)log10(i * 1.0) + 1; 
+    index -= group[i];
+  } // for
+
+  /* That the for loop stops means that the traget digit is in
+   * group[i] with the updated index [1, group[i]]
+   * */
+  index += group[--i];
+
+  /* Now, search the digit with the corresponding index in group[i].
+   * */
+  int j;
+  for (j = 1; group[j] < index; j++)
+    ;
+
+  /* The digit should be some digit of the integer j*/
+  index -= group[j - 1];
+  int n = (int)log10(j * 1.0) + 1 - index + 1;
+  /* reverse index for mod op: for a 4-digit num, index = 1 => n = 4 */
+  while (n > 0) {
+    j %= 10;
+    n--;
   }
 
-  int index = 19;
-  for (i = 1, acc = 0; acc < index; i++)
-    acc += group[i];
-
-  index -= group[--i];
+  printf("%d\n", j);
 
 }

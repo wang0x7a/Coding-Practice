bc465a35e5f310f4ad15fd17c3c6426cee3fd581
zw267 <zhexwang@gmail.com>
2014-02-13 15:18:39 +0800

    AC version with 2000ms limit on openjudge

--- dsalgo/01-linear-lists/p0201.c
+++ dsalgo/01-linear-lists/p0201.c
@@ -7,7 +7,7 @@
 #include <math.h>
 
 #define MAX_DICT_LEN 10000
-#define MAX_WORD_LEN 20 
+#define MAX_WORD_LEN 15 
 
 typedef char *Word;
 typedef Word *Dict; 
@@ -22,11 +22,12 @@ int min3(int a, int b, int c);
 int main() {
   Dict dict = build_dict();
 
-  while(1) {
+  //while(1) {
+  while (1) {
     Word word = malloc(sizeof(char) * MAX_WORD_LEN);
     scanf("%s", word);
 
-    if (*word == '#')
+    if (word[0] == '#')
       break;
 
     Dict cand = lookup(word, dict);
@@ -91,18 +92,21 @@ Dict lookup(Word word, Dict dict) {
 int calc_dist(Word a, Word b) {
   int len_a = strlen(a);
   int len_b = strlen(b);
+  
+  if (abs(len_a - len_b) > 1) return 2;
 
   return calc_dist_helper2(a, len_a, b, len_b);
 }
 
 int calc_dist_helper2(Word a, int len_a, Word b, int len_b) {
   int d[1 + len_a][1 + len_b];
+  d[0][0] = 0;
 
   // init d
   int i, j;
-  for (i = 0; i <= len_a; i++)
-    for (j = 0; j <= len_b; j++)
-      d[i][j] = 0;
+  //for (i = 0; i <= len_a; i++)
+  //  for (j = 0; j <= len_b; j++)
+  //    d[i][j] = 0;
 
   for (i = 1; i <= len_a; i++) 
     d[i][0] = i;
@@ -112,17 +116,22 @@ int calc_dist_helper2(Word a, int len_a, Word b, int len_b) {
 
   for (j = 1; j <= len_b; j++) {
     for (i = 1; i <= len_a; i++) {
-      if (d[i][j] > 1) return 2;
-      else {
-        //printf("a[i]:%c, b[j]:%c\n", a[i], b[j]);
+      //printf("a[i]:%c, b[j]:%c\n", a[i], b[j]);
+      d[i][j] = 0;
+      if (d[i - 1][j - 1] <= 1) {
         if (a[i - 1] == b[j - 1])
           d[i][j] = d[i - 1][j - 1];
         else
           d[i][j] = min3(
-              d[i - 1][j] + 1,
-              d[i][j - 1] + 1,
-              d[i - 1][j - 1] + 1
+              d[i - 1][j] + 1,       // insert at a[i - 1]
+              d[i][j - 1] + 1,       // delete a[i]
+              d[i - 1][j - 1] + 1    // replace a[i]
               );
+
+        if (d[i][j] > 1) d[i][j] = 2;
+      }
+      else {
+        d[i][j] = 2;
       }
     }
   }

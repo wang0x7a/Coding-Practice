bd1ede9517bf53c5da19a56f389ded7d8071f506
zw267 <zhexwang@gmail.com>
2014-02-18 12:33:25 +0800

    a buggy version

---
+++ algs/my_kmp.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef char *String;
+
+void pre_kmp(char *pat, int pat_len, int *lps);
+void kmp_search(char *pat, char *txt);
+
+int main() {
+  char *txt = "ABABDABACDABABCABAB";
+  char *pat = "ABABCABAB";
+  kmp_search(pat, txt);
+  return 0;
+}
+
+void kmp_search(char *pat, char *txt) {
+  int pat_len = strlen(pat);
+  int txt_len = strlen(txt);
+
+  int *lps = malloc(sizeof(int) * pat_len);
+  pre_kmp(pat, pat_len, lps);
+
+  int i = 0;    // index for txt
+  int j = 0;    // index for pat
+  while (i < txt_len) {
+    /*
+    if (j == pat_len + 1) {
+      j = 0;
+      i++;
+      printf("Find pattern in txt at: %d", i - j - 1);
+      continue;
+    }
+
+    if (txt[i] != pat[j])
+      j = lps[j];
+    else
+      j++;
+
+    i++;
+  }
+  */
+  if (pat[i] == pat[j]) {
+    i++;
+    j++;
+  }
+  
+  if (j > pat_len) {
+    printf("Find pattern in txt at %d\n", i - pat_len);
+    j = 0;
+  }
+
+  if (pat[i] != pat[j]) {
+    j = lps[j - 1]
+  }
+
+  return;
+}
+
+void pre_kmp(char *pat, int pat_len, int *lps) {
+  int len = 0;     // the length of the previous longest prefix
+  int i;
+
+  lps[0] = 0;
+  i = 1;
+
+  while (i < pat_len) {
+    if (pat[i] == pat[len]) {
+      len++;
+      pat[i] = len;
+      i++;
+    }
+    else {
+      if (len != 0) {
+        len = lps[len - 1];
+      }
+      else {
+        lps[i] = 0;
+        i++;
+      }
+    }
+  }
+}

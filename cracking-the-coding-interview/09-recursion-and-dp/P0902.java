04575b5cea41d18acd8f92d1c055df502d24bebe
zw267 <zhexwang@gmail.com>
2014-03-27 11:44:57 +0800

    partial solution to P0902

--- cracking-the-coding-interview/09-recursion-and-dp/P0902.java
+++ cracking-the-coding-interview/09-recursion-and-dp/P0902.java
@@ -8,3 +8,19 @@
  * on them. Design an algorithm to find a path for the robot from the top left
  * to the bottom right.
  * */
+
+/* Solution:
+ *
+ * 1) The number of possible paths from (0, 0) to (X, Y):
+ *    C^X_(X+Y)
+ *
+ *    The probability of finding a right path:
+ *    C^X_(X+Y)
+ *    ---------
+ *     2^(X+Y)
+ *
+ * 2) Key words: find a path
+ *    Questions: are the "off limits" spots known in advance? Or, the robot
+ *    won't know until it reaches the spots?
+ *
+ * */

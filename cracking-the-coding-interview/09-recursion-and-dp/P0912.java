d4bd616cf086ff4c81716ef9aa4cb67eb0cbbbad
wang0x7a <zhexwang@gmail.com>
2014-03-31 11:12:48 +0800

    init the problem of edit distance

---
+++ cracking-the-coding-interview/09-recursion-and-dp/P0912.java
@@ -0,0 +1,11 @@
+/* P0912:
+ * Edit distance between two strings
+ * */
+
+public class P0912 {
+  public static int editDist(String s1, String s2) {
+    return 0;
+  }
+
+  public static void main(String[] args) {}
+}

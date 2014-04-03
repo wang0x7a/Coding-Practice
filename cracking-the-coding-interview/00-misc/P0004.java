d61f494b76092e5bd4b4594d8db1ef07d740f467
wang0x7a <zhexwang@gmail.com>
2014-04-03 12:43:10 +0800

    a DP version but fail with abcdba

--- cracking-the-coding-interview/00-misc/P0004.java
+++ cracking-the-coding-interview/00-misc/P0004.java
@@ -45,6 +45,44 @@ public class P0004 {
     return true;
   }
 
+  // Finding the longest palindromic substring in a string is equivalent to
+  // retrieving the longest common sequence between this string and its reverse.
+  public static int lpsDP(String str) {
+    int res = 0;
+    
+    int len = str.length();
+    int[][] r = new int[len + 1][len + 1];
+    
+    String rev = new StringBuilder(str).reverse().toString();
+
+    res = lpsDPHelper(str, rev, r);
+
+    return res;
+  }
+
+  public static int lpsDPHelper(String str, String rev, int[][] r) {
+    int max = 0;
+
+    int n = r.length;
+    int len = n - 1;
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < n; j++) {
+        if (str.charAt(i - 1) == rev.charAt(j - 1)) {
+          r[i][j] = r[i - 1][j - 1] + 1;
+
+          if ((r[i][j] > max) && (i + j > len))
+            max = r[i][j];
+        }
+        else {
+          r[i][j] = 0;
+        }
+      }
+    }
+
+
+    return max;
+  }
+
   public static void main(String[] args) {
     String[] strs = {"abccba", "a", "abcdba", "abdb", "abababa"};
 
@@ -53,6 +91,7 @@ public class P0004 {
 
       //System.out.println(s + ": " + isPalindromicX(0, len - 1, s));
       System.out.println(s + ": " + lpsRec(s));
+      System.out.println(s + ": " + lpsDP(s));
     }
   }
 }

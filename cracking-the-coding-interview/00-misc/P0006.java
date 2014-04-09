db3e104dc85460d0b81d685642546abbcbdc624a
wang0x7a <zhexwang@gmail.com>
2014-04-10 00:15:52 +0800

    DFS solution to P0006

--- cracking-the-coding-interview/00-misc/P0006.java
+++ cracking-the-coding-interview/00-misc/P0006.java
@@ -13,7 +13,8 @@
  * start = "hit"
  * end = "cog"
  * dict = ["hot", "dot", "dog", "lot", "log"]
- * As one shortest transformation is "hit" -> "hot" -> "dot" -> "dot" -> "cog",
+ * As one shortest transformation is
+ * "hit" -> "hot" -> "lot" -> "log" -> "cog",
  * return its length 5.
  *
  * Note:
@@ -26,12 +27,15 @@ import java.util.HashSet;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Iterator;
 
 public class P0006 {
 
   @SuppressWarnings("unchecked")
   public static int wordLadder(String start, String end, HashSet<String> dict) {
-    int res = 0;
+    // by default, level 0 only contains start. If there is a chain of, therei
+    // will be at least one transformation.
+    int res = 1;
 
     // use a copy of the dictionary
     dict = (HashSet<String>)dict.clone();
@@ -39,16 +43,41 @@ public class P0006 {
     Queue<String> queue = new LinkedList<String>();
     queue.offer(start);
 
-    int level = 0;
+    int[] level = new int[2];
+    int i = res % 2; 
+    // i = 0;
+    // i is the index of the level that we are dequeuing
+    level[i] = 1;
+    int j = (i + 1) % 2;
+    // j = 1;
+    // j is the index of the level we are queuing
+
     while (queue.size() > 0) {
       String current = queue.poll();
-      for (String cand : dict) {
+      level[i]--;
+
+      for (Iterator<String> iter = dict.iterator(); iter.hasNext(); ) {
+        String cand = iter.next();
+        
+
         if (isValidTrans(current, cand)) {
+          queue.offer(cand);
+          iter.remove();
+          level[j]++;
+
+          if (isValidTrans(end, cand))
+            // wrong: return res++;
+            return ++res;
         }
+      }
+      if (level[i] == 0) {
+        res++;
+        i = res % 2;
+        j = (i + 1) % 2;
       }
     }
 
-    return res;
+    return 0;
   }
 
   private static boolean isValidTrans(String src, String tgt) {
@@ -68,7 +97,8 @@ public class P0006 {
 
   public static void main(String[] args) {
     String start = "hit", end = "cog";
-    String[] dictArray = new String[]{"hot", "dot", "dog", "lot", "log"};
+    //String[] dictArray = new String[]{"hot", "dot", "dog", "lot", "log"};
+    String[] dictArray = new String[]{"hot", "dot", "lot"};
     HashSet<String> dict = new HashSet<String>(Arrays.asList(dictArray));
 
     int res = P0006.wordLadder(start, end, dict);

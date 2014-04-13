40fe42884844cfe0a699d66ab248facf009093e4
zw267 <zhexwang@gmail.com>
2014-04-13 20:01:46 +0800

    solution to P0010

--- cracking-the-coding-interview/00-misc/P0010.java
+++ cracking-the-coding-interview/00-misc/P0010.java
@@ -8,10 +8,25 @@
  * Example:
  * Input : numbers = {2, 7, 11, 15}
  *         target  = 9
- * Output: index1 = 1, index2 = 3
+ * Output: index1 = 1, index2 = 2
  */
 
 public class P0010 {
+    public static void twoSum(int[] numbers, int target) {
+        int len = numbers.length;
+
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (numbers[i] + numbers[j] == target)
+                    System.out.println("index1 = " + (j + 1) + ", index2 = " + (i + 1));
+            }
+        }
+    }
+
     public static void main(String args[]) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+        
+        P0010.twoSum(numbers, target);
     }
 }

ad0f6d5453d08d6cc6c8d1246c2170bdfdc03fb7
wang0x7a <zhexwang@gmail.com>
2014-04-13 13:34:40 +0800

    description of P0010

---
+++ cracking-the-coding-interview/00-misc/P0010.java
@@ -0,0 +1,17 @@
+/**
+ * P0010
+ *
+ * Two Sum
+ * Given an array of intergers, find two numbers such that they add up to a
+ * specific target number.
+ *
+ * Example:
+ * Input : numbers = {2, 7, 11, 15}
+ *         target  = 9
+ * Output: index1 = 1, index2 = 3
+ */
+
+public class P0010 {
+    public static void main(String args[]) {
+    }
+}

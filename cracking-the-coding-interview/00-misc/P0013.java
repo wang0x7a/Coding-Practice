0e13944d6f78c8bb49d1e1f93b06a24b5f6a52e1
zw267 <zhexwang@gmail.com>
2014-04-14 22:48:37 +0800

    untested solution to P0013

--- cracking-the-coding-interview/00-misc/P0013.java
+++ cracking-the-coding-interview/00-misc/P0013.java
@@ -7,3 +7,38 @@
  * 1) Allow to use an auxilary array to store the restul? (O(M+N) space)
  * 2) What if the space complexity is O(1) (without additional storage)?
  */
+
+public class P0013 {
+    public static void merge(int[] a, int m, int[] b, int n) {
+        // a.length = m + n
+        // b.length = n
+        int len = a.length;
+
+        int i = m - 1, j = n - 1;
+        for (int k = len - 1; k >= 0; k--) {
+            if (i > 0 && j < 0)
+                a[k] = a[i--];
+            else if (i < 0 && j > 0)
+                a[k] = b[j--];
+            else {
+                if (a[i] > b[j]) {
+                    a[k] = a[i--];
+                }
+                else {
+                    a[k] = b[j--];
+                }
+            }
+        }
+    }
+
+    public static void print(int[] a, int len) {
+        for (int i = 0; i < len; i++)
+            System.out.print(a[i] + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int m = 5, n = 5;
+    }
+}

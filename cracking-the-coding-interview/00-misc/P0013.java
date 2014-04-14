69d8e77a4574ac227029e870f000341e409692e6
zw267 <zhexwang@gmail.com>
2014-04-14 16:41:16 +0800

    description to P0013

---
+++ cracking-the-coding-interview/00-misc/P0013.java
@@ -0,0 +1,9 @@
+/* P0013
+ *
+ * Merge Sorted Array
+ * Given two sorted integer arrays A and B, merge B into A as one sorted array.
+ *
+ * Notes:
+ * 1) Allow to use an auxilary array to store the restul? (O(M+N) space)
+ * 2) What if the space complexity is O(1) (without additional storage)?
+ */

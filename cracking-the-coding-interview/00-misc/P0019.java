f0b175ff1fdf99365e3217a23612a49cc5da37d1
zw267 <zhexwang@gmail.com>
2014-04-16 16:20:35 +0800

    solution to P0019

--- cracking-the-coding-interview/00-misc/P0019.java
+++ cracking-the-coding-interview/00-misc/P0019.java
@@ -4,3 +4,41 @@
  * Given a linked list, determine if it has a cycle in it.
  */
 
+public class P0019 {
+    public static boolean isCircular(ListNode list) {
+        if (list == null || list.next == null)
+            return false;
+
+        ListNode slow = list;
+        ListNode fast = list.next.next;
+
+        while (fast != null) {
+            if (slow == fast)
+                return true;
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+    }
+
+    public static void main(String args[]) {}
+}
+
+class ListNode {
+    int value;
+    ListNode next;
+
+    public ListNode() {}
+
+    public ListNode(int value) {
+        this.value = value;
+        this.next  = null;
+    }
+
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next  = null;
+    }
+}

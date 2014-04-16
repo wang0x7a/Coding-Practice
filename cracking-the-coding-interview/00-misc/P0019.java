2ee91b378bfd4402f3474e75c768085525955d85
zw267 <zhexwang@gmail.com>
2014-04-16 16:30:42 +0800

    P0019 minor fix and add test cases

--- cracking-the-coding-interview/00-misc/P0019.java
+++ cracking-the-coding-interview/00-misc/P0019.java
@@ -10,20 +10,32 @@ public class P0019 {
             return false;
 
         ListNode slow = list;
-        ListNode fast = list.next.next;
+        ListNode fast = list;
 
-        while (fast != null) {
-            if (slow == fast)
-                return true;
-
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
         }
 
         return false;
     }
 
-    public static void main(String args[]) {}
+    public static void main(String args[]) {
+        ListNode head = new ListNode(1);
+        ListNode n1 = new ListNode(2);
+        head.next = n1;
+        ListNode n2 = new ListNode(3);
+        n1.next = n2;
+        ListNode n3 = new ListNode(4);
+        n2.next = n3;
+
+        //n3.next = n1;
+
+        System.out.println(isCircular(head));
+    }
 }
 
 class ListNode {

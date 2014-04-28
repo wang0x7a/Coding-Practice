d73c13f981b9768528e2eecc895aef5ab76b5642
zw267 <zhexwang@gmail.com>
2014-04-28 12:21:15 +0800

    imporved solution to P1703

--- cracking-the-coding-interview/17-moderate/P1703.java
+++ cracking-the-coding-interview/17-moderate/P1703.java
@@ -31,10 +31,30 @@ public class P1703 {
         return count;
     }
 
+    public static int trailingZerosX(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++)
+            count += factorsOf5(i);
+
+        return count;
+    }
+
+    private static int factorsOf5(int n) {
+        int count = 0;
+
+        while (n % 5 == 0) {
+            count++;
+            n /= 5;
+        }
+
+        return count;
+    }
+
     public static void main(String args[]) {
         int n = Integer.parseInt(args[0]);
 
-        int res = trailingZeros(n);
+        int res = trailingZerosX(n);
 
         System.out.println(res);
     }

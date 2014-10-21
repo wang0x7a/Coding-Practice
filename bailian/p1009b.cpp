3b849a61ec11ae67fdc61dfc641799d9a67ca8bf
zw267 <zhexwang@gmail.com>
2014-10-21 16:08:23 +0800

    revisit p1009

--- bailian/p1009b.cpp
+++ bailian/p1009b.cpp
@@ -6,9 +6,27 @@ using namespace std;
 
 #define MAX_PAIR_NUM 1000
 
+int pixel_pair[MAX_PAIR_NUM][2];
+int img_width;
+
 int main() {
-  cout << "test";
-  cout << "test2";
-  cout << "test3";
-  cout << "test4";
+  while (cin >> img_width && img_width > 0) {
+    int num, pixel;
+    int pair_cnt  = 0;
+    int pixel_cnt = 0; 
+
+    while (cin >> pixel >> num && num != 0 && pixel != 0) {
+      pixel_pair[pair_cnt][0] = pixel; 
+      pixel_pair[pair_cnt][1] = num;
+
+      pair_cnt++;
+
+      pixel_cnt += num;
+    }
+
+    for (int i = 0; i < pair_cnt; i++)
+      cout << pixel_pair[i][0] << " " << pixel_pair[i][1] << endl;
+
+    cout << pixel_cnt << endl;
+  }
 }

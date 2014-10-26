f04ab31cd4a066be616c1e8dff49beff38af7bde
zw267 <zhexwang@gmail.com>
2014-10-26 22:48:53 +0800

    p1018

--- bailian/p1018.cpp
+++ bailian/p1018.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cstring>
+#include <climits>
 
 #define MAX_DEV_NUM 10000
 
@@ -17,21 +18,19 @@ bool cmp(const Device& a, const Device& b);
 
 int main() {
   int t;
-  
   cin >> t;
 
   for (int i = 0; i < t; i++) {
-    int n;
-    cin >> n;
+    int numOfDevs;
+    cin >> numOfDevs;
 
     Device dev[MAX_DEV_NUM];
     int numOfOpts = 0;
-    int numOfDevs = 0;
-    for (int j = 1; j <= n; j++) {
-      cin >> numOfDevs;
-
+    for (int j = 1; j <= numOfDevs; j++) {
       int b, p;
-      for (int cnt = 0; cnt < numOfDevs; cnt++) {
+      int m;
+      cin >> m;
+      for (int cnt = 0; cnt < m; cnt++) {
         cin >> b >> p;
 
         dev[numOfOpts].id        = j;
@@ -44,12 +43,63 @@ int main() {
 
     sort(dev, dev + numOfOpts, cmp);
 
-    bool isVisited[numOfDevs];
-    memset(isVisited, false, sizeof(isVisited));
+    int bandwidth, totalPrice;
+    float result = 0.0, tmp;
+    //memset(price, INT_MAX, sizeof(price));
+    for (int i = 0; i <= numOfOpts - numOfDevs; i++) {
+      int price[numOfDevs];
+      memset(price, CHAR_MAX, sizeof(price));
+
+      bool isVisited[numOfDevs];
+      memset(isVisited, false, sizeof(isVisited));
+      bandwidth = dev[i].bandwidth;
+      price[dev[i].id - 1] = dev[i].price;
+      isVisited[dev[i].id - 1] = true;
+
+      for (int j = i + 1; j <= numOfOpts - 1; j++) {
+
+        if (dev[j].id == dev[i].id)
+          continue;
+
+        if (price[dev[j].id - 1] > dev[j].price) {
+          price[dev[j].id - 1] = dev[j].price;
+          isVisited[dev[j].id - 1] = isVisited[dev[j].id - 1] || true;
+        }
+      }
+
+      bool isAllVisited = true;
+      for (int i = 0; i < numOfDevs; i++) {
+        if (!isVisited[i]) {
+          isAllVisited = false;
+          break;
+        }
+      }
+
+
+      if (!isAllVisited)
+        tmp = 0.0;
+      else {
+        for (int i = 0; i < numOfDevs; i++)
+          totalPrice += price[i];
+
+        tmp = bandwidth * 1.0 / totalPrice;
+      }
+
+      if (tmp > result) {
+        for (int i = 0; i < numOfDevs; i++)
+          cout << price[i] << " ";
+        cout << endl;
+        cout << bandwidth << endl;
+        result = tmp;
+      }
+    }
+
+    cout << result << endl;
 
     for (int i = 0; i < numOfOpts; i++)
       cout << dev[i].bandwidth << "\t" << dev[i].price << "\t" 
         << dev[i].id << endl;
+
   }
 }
 

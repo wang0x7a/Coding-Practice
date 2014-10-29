17814175df6392cc6f5e77af6eb7a733a8cef022
zw267 <zhexwang@gmail.com>
2014-10-29 20:51:52 +0800

    implement flip func

--- bailian/p1753.cpp
+++ bailian/p1753.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+int flip(int a, int pos);
+void print(int a) {
+  for (int i = 0; i <= 15; i++) {
+    int tmp = a;
+    cout << ((tmp >> i) & 1) << " ";
+
+    if (i % 4 == 3)
+      cout << endl;
+  }
+  cout << endl;
+}
+
 int main() {
   int a = 0;
 
@@ -21,10 +33,44 @@ int main() {
   }
 
   /*
-  for (int i = 0; i <= 15; i++) {
-    int tmp = a;
-    cout << ((tmp >> i) & 1) << " ";
-  }
-  cout << endl;
+  int b = flip(a, 0);
+  int c = flip(a, 4);
+  int d = flip(a, 3);
+  int e = flip(a, 6);
+  int f = flip(a, 15);
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(e);
+  print(f);
   */
+}
+
+int flip(int a, int pos) {
+  int row = pos / 4;
+  int col = pos % 4;
+
+  for (int i = row - 1; i <= row + 1; i++)
+    for (int j = col - 1; j <= col + 1; j++) {
+      if (i < 0 || i > 3 || j < 0 || j > 3)
+        continue;
+
+      if (((i == row - 1) && (j == col - 1)) 
+          || ((i == row - 1) && (j == col + 1))
+          || ((i == row + 1) && (j == col - 1))
+          || ((i == row + 1) && (j == col + 1)))
+        continue;
+
+      int shift = i * 4 + j;
+
+      int mask = 1 << shift;
+
+      if (mask & a)
+        a = (~mask) & a;
+      else
+        a = mask | a;
+    }
+
+  return a;
 }

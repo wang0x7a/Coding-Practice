ae593921c173af0165984823460870a8dce20e85
zw267 <zhexwang@gmail.com>
2014-11-14 17:44:27 +0800

    p1328 AC

---
+++ bailian/p1328b.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+#define MAX_RADAR_NUM 1000
+
+using namespace std;
+
+typedef struct Point {
+  float x;
+  float y;
+} Point;
+
+typedef struct Interval {
+  float left;
+  float right;
+} Interval;
+
+bool cmp(Point a, Point b);
+int solve(Point p[], int n, float d);
+
+int main() {
+  int n;
+  float d;
+
+  int caseNum = 0;
+  while (cin >> n >> d) {
+    if (n == 0 && d == 0.0)
+      break;
+
+    caseNum++;
+
+    Point p[n];
+    for (int i = 0; i < n; i++)
+      cin >> p[i].x >> p[i].y;
+
+    sort(p, p + n, cmp);
+
+    int radarNum = solve(p, n, d);
+
+    cout << "Case " << caseNum << ": " << radarNum << endl;
+
+    /*
+    for (int i = 0; i < n; i++)
+      cout << p[i].x << " " << p[i].y << endl;
+    */
+  }
+}
+
+bool cmp(Point a, Point b) {
+  if (a.x != b.x)
+    return a.x < b.x;
+  else
+    return a.y < b.y;
+} 
+
+int solve(Point p[], int n, float d) {
+  int radarNum = 1;
+
+  Interval intvl[n];
+
+  for (int i = 0; i < n; i++) {
+    if (p[i].y > d)
+      return -1;
+
+    float vertDist = sqrt(d * d - p[i].y * p[i].y);
+    intvl[i].left  = p[i].x - vertDist;
+    intvl[i].right = p[i].x + vertDist;
+  }
+
+  Interval merged = intvl[0];
+  Interval tmp;
+  for (int i = 1; i < n; i++) {
+    tmp.left  = max(merged.left, intvl[i].left);
+    tmp.right = min(merged.right, intvl[i].right);
+
+    if (tmp.left > tmp.right) {
+      radarNum++;
+      merged = intvl[i];
+    }
+    else
+      merged = tmp;
+  }
+
+  return radarNum;
+}

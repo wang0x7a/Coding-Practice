2a1a36eb0a7423050b47413e2b32e963e4f8e259
zw267 <zhexwang@gmail.com>
2014-12-16 15:25:58 +0800

    p1011 solving

--- bailian/p1011.cpp
+++ bailian/p1011.cpp
@@ -7,9 +7,14 @@ using namespace std;
 int pieces[MAX_PIECE_NUM];
 int piece_num;
 int piece_len_sum;
+unsigned long all_visited;
 
-int solve();
+
+void solve();
+void solve_helper(int tgt_value, int curr_sum, int rest_sum, 
+    int curr_grp, int visited, bool& res);
 void print();
+unsigned long get_all_visited(int piece_num);
 
 int main() {
   int n;
@@ -19,25 +24,60 @@ int main() {
     piece_num = n;
     piece_len_sum = 0;
 
+    all_visited = get_all_visited(piece_num);
+
     for (int i = 0; i < piece_num; i++) {
       cin >> n;
       pieces[i] = n;
       piece_len_sum += n;
     }
 
-    res = solve();
+    solve();
 
     cout << res << endl;
     //print();
   }
 }
 
-int solve() {
-  int res;
+unsigned long get_all_visited(int piece_num) {
+  unsigned long res = 0;
 
-  sort(pieces, pieces + piece_num);
+  for (int i = 0; i < piece_num; i++)
+    res = res || (1 << i);
 
   return res;
+}
+
+void solve() {
+  sort(pieces, pieces + piece_num);
+
+  int tgt_value = pieces[piece_num - 1];
+  bool res = false;
+  for (int tgt_value = pieces[piece_num - 1]; tgt_value < piece_len_sum;
+      tgt_value++) {
+
+    if (piece_len_sum % tgt_value)
+      continue;
+
+    solve_helper(tgt_value, 0, piece_len_sum, 0, 0, res);
+    if (res) {
+      cout << tgt_value << endl;
+      break;
+    }
+  }
+
+  return;
+}
+
+void solve_helper(int tgt_value, int curr_sum, int rest_sum, 
+    int curr_grp, int visited, bool& res) {
+
+  if (rest_sum % tgt_value) {
+    res = false;
+    return;
+  }
+
+  return;
 }
 
 void print() {

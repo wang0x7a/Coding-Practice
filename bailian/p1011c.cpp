574d627cc35fb209b3399600516062bad7206385
zw267 <zhexwang@gmail.com>
2014-12-22 10:15:46 +0800

    p1011c pruned still TLE

--- bailian/p1011c.cpp
+++ bailian/p1011c.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <algorithm>
-#include <string>
 
 #define MAX_PIECE_NUM 64
 
@@ -9,12 +8,11 @@ using namespace std;
 int pieces[MAX_PIECE_NUM];
 int piece_num;
 int piece_len_sum;
-int is_visited[MAX_PIECE_NUM];
 
 void solve();
 
-int get_next_idx(int idx, int tgt_value, int acc);
-bool dfs(int idx, int tgt_value, int acc, int rest);
+int get_next_idx(int idx, unsigned long visited, int tgt_value, int acc);
+bool dfs(int idx, int tgt_value, int acc, unsigned long visited, int rest);
 
 void print();
 
@@ -25,7 +23,6 @@ int main() {
     piece_num = n;
     piece_len_sum = 0;
 
-    memset(is_visited, false, sizeof(is_visited));
     for (int i = 0; i < piece_num; i++) {
       cin >> n;
       pieces[i] = n;
@@ -55,7 +52,7 @@ void solve() {
     if (piece_len_sum % tgt_value)
       continue;
 
-    res = dfs(piece_num - 1, tgt_value, 0, piece_len_sum);
+    res = dfs(piece_num - 1, tgt_value, 0, 0, piece_len_sum);
 
     if (res) {
       cout << tgt_value << endl;
@@ -66,47 +63,49 @@ void solve() {
   return;
 }
 
-bool dfs(int idx, int tgt_value, int acc, int rest) {
+bool dfs(int idx, int tgt_value, int acc, unsigned long visited, int rest) {
   if (rest == 0)
     return true;
 
   if (acc == tgt_value)
-    return true && dfs(piece_num - 1, tgt_value, 0, rest - acc);
+    return true && dfs(piece_num - 1, tgt_value, 0, visited, rest - acc);
 
   if (idx < 0 || tgt_value < acc)
     return false;
 
-  bool res;
+  bool res1 = false;
   int next_idx;
 
-  next_idx = get_next_idx(idx, tgt_value, acc);
+  next_idx = get_next_idx(idx, visited, tgt_value, acc);
 
-  res = dfs(next_idx, tgt_value, acc, rest);
+  res1 = dfs(next_idx, tgt_value, acc, visited, rest);
 
-  if (res)
-    return true;
-
-  if (!is_visited[idx]) {
-    is_visited[idx] = true;
+  unsigned long mask = 1 << idx;
+  if ((mask & visited) == 0) {
+    visited = visited | mask;
 
     acc += pieces[idx];
   }
 
-  next_idx = get_next_idx(idx, tgt_value, acc);
+  next_idx = get_next_idx(idx, visited, tgt_value, acc);
 
-  res = dfs(next_idx, tgt_value, acc, rest);
+  bool res2 = dfs(next_idx, tgt_value, acc, visited, rest);
 
-  return res;
+  return res1 || res2;
 } 
 
-int get_next_idx(int idx, int tgt_value, int acc) {
+int get_next_idx(int idx, unsigned long visited, int tgt_value, int acc) {
   int next_idx = idx - 1;
+  unsigned long mask = 1 << (idx - 1);
 
   while (next_idx >= 0) {
-    if (acc + pieces[next_idx] <= tgt_value && !is_visited[next_idx])
+    unsigned long is_visited = mask & visited;
+
+    if (acc + pieces[next_idx] <= tgt_value && is_visited == 0)
       break;
 
     next_idx--;
+    mask = mask << 1;
   }
 
   return next_idx;

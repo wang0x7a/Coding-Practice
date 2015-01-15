54fe544da3cd5f6402a4d29999834e03fc69b2dd
zw267 <zhexwang@gmail.com>
2015-01-15 22:58:38 +0800

    implemented LRUCache using LinkedHashMap

---
+++ data-structures/LRUCache.java
@@ -0,0 +1,76 @@
+import java.util.LinkedHashMap;
+import java.util.Collection;
+import java.util.Map;
+import java.util.ArrayList;
+
+public class LRUCache<K, V> {
+    private static final float hashTableLoadFactor = 0.75f;
+
+    private LinkedHashMap<K, V> map;
+    private int cacheSize;
+
+    public LRUCache(int cacheSize) {
+        this.cacheSize = cacheSize;
+        int hashTableCapacity = 
+            (int)Math.ceil(cacheSize / hashTableLoadFactor) + 1;
+
+        map = new LinkedHashMap<K, V>(hashTableCapacity, 
+                hashTableLoadFactor, true) {
+            private static final long serialVersionUID = 1;
+
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                return size() > LRUCache.this.cacheSize;
+            }
+        };
+    }
+
+    public synchronized V get(K key) {
+        return map.get(key);
+    }
+
+    public synchronized void put(K key, V value) {
+        map.put(key, value);
+    }
+
+    public synchronized void clear() {
+        map.clear();
+    }
+
+    public synchronized int usedEntries() {
+        return map.size();
+    }
+
+    public synchronized Collection<Map.Entry<K, V>> getAll() {
+        return new ArrayList<Map.Entry<K, V>>(map.entrySet());
+    }
+
+    public static void main(String[] args) {
+        LRUCache<String, String> c = new LRUCache<String, String>(3);
+
+        c.put("1", "one");
+        c.put("2", "two");
+        c.put("3", "three");
+        c.put("4", "four");
+        if (c.get("2") == null)
+            throw new Error();
+
+        c.put("5", "five");
+        c.put("4", "second four");
+
+        if (c.usedEntries() != 3)
+            throw new Error();
+
+        if (!c.get("4").equals("second four"))
+            throw new Error();
+
+        if (!c.get("5").equals("five"))
+            throw new Error();
+
+        if (!c.get("2").equals("two"))
+            throw new Error();
+
+        for (Map.Entry<String, String> e : c.getAll())
+            System.out.println(e.getKey() + " : " + e.getValue());
+    }
+}

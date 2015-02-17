c83ece8341cfaab73af2393bbf0860d63363b5f3
zw267 <zhexwang@gmail.com>
2015-02-17 11:23:39 +0800

    init p1012 & p1013

---
+++ bailian/p1012.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int solve(int k);
+
+int main() {
+  int k;
+  int m;
+
+  while (cin >> k && k) {
+    m = solve(k);
+
+    cout << m << endl;
+  }
+}
+
+int solve(int k) {
+  return 0;
+}
---
+++ bailian/p1013.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef struct {
+  string left;
+  string right;
+  int    result;
+} Record;
+
+int main() {
+  int n;
+
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+  }
+}
